806d52f02d17538d65757ae0fda48bcf6e318508
Tomáš Vostiňák <tomas.vostinak@tul.cz>
2026-01-09 22:15:28 +0100

    Update clean_html function in ingestion.py, updated requirements

--- ingestion.py
+++ ingestion.py
@@ -4,18 +4,35 @@
 
 import requests
 import time
+import bs4
 
 URL_FILE = "data/project-urls.txt"
 
 def fetch_html(url):
     response = requests.get(url, timeout=10)
     if response.ok:
+        response.encoding = response.apparent_encoding
         return response.text
     else:
         print(f"Error fetching {url}: {response.status_code}")
 
 def clean_html(html):
-    return 0
+    soup = bs4.BeautifulSoup(html, "html.parser")
+    main = soup.body
+
+    for tag in main(["script", "style", "header", "footer", "nav", "aside"]):
+        tag.decompose()
+
+    text = main.get_text(separator="\n", strip=True)
+    lines = []
+    for line in text.splitlines():
+        line = line.strip()
+        if not line or len(line.split()) <= 5:
+            continue
+        lines.append(line)
+    text = "\n".join(lines)
+
+    return text
 
 def ingest_urls():
     with open(URL_FILE, "r", encoding="utf-8") as file:
@@ -29,8 +46,9 @@ def ingest_urls():
     
     time.sleep(0.5)
 
-#fetch_html("https://cs.wikipedia.org/wiki/Jablonec_nad_Nisou")
-
 if __name__ == "__main__":
-    ingest_urls()
+    #ingest_urls()
+    html = fetch_html("https://knihovna.mestojablonec.cz/")
+    text = clean_html(html)
+    print(text)
     pass
